2198fbd699ee1a99f315737748db2a499f48e30b
Mark <markwilkins86@gmail.com>
2022-05-14 11:36:20 -0400

    commit tree 308306622fd69aa19794e947c80c52eeea16b2f2

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your code here
+class Breakfast {
+    constructor(food, drink) {
+    this.food = food
+    this.drink = drink
+    }
+}
+
+let newBreakfast = new Breakfast('cereal', 'OJ')
+
+class Lunch {
+    constructor(salad, soup, drink) {
+        this.salad = salad
+        this.soup = soup
+        this.drink = drink
+    }
+}
+
+let newLunch = new Lunch('ceasar', 'tomato', 'soda')
+
+class Dinner {
+    #dessert
+    constructor(salad, soup, entree, dessert) {
+        this.salad = salad
+        this.soup = soup 
+        this.entree = entree 
+        this.#dessert = dessert 
+
+    }
+}
+
+let newDinner = new Dinner('ice cream', 'cobb', 'bisque', 'steak')
